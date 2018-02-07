8ba9b9c57ccea21652fc9e8ad3c84544bced9a09
Ross Jennings <rossjjennings@gmail.com>
2018-02-07 14:48:32 -0500

    Reorganized into class weight_fn

--- cubit/moments.py
+++ cubit/moments.py
@@ -1,32 +1,32 @@
 import sympy as sym
 
 x = sym.Symbol('x')
-
-def inner(p1, p2, momfunc):
-    prod = sym.Poly(p1*p2, x)
-    iter = enumerate(reversed(prod.all_coeffs()))
-    return sum(coeff*momfunc(i) for i, coeff in iter)
-
-def sqnorm(p, momfunc):
-    prod = sym.Poly(p**2, x)
-    iter = enumerate(reversed(prod.all_coeffs()))
-    return sum(coeff*momfunc(i) for i, coeff in iter)
-
-def orthopoly(momfunc):
-    def momfunc_orthopoly(n, cache = {}):
+class weight_fn:
+    def __init__(self, momfunc):
+        self.moment = momfunc
+    
+    def inner(p1, p2):
+        prod = sym.Poly(p1*p2, x)
+        iter = enumerate(reversed(prod.all_coeffs()))
+        return sum(coeff*self.moment(i) for i, coeff in iter)
+    
+    def sqnorm(p):
+        return self.inner(p, p)
+    
+    def orthopoly(self, n, cache = {}):
         try:
             return cache[n]
         except KeyError:
             if n == 0:
                 cache[n] = sym.Integer(1)
             elif n == 1:
-                cache[n] = x - momfunc(1)/momfunc(0)
+                cache[n] = x - self.moment(1)/moment(0)
             else:
-                twoprev = momfunc_orthopoly(n - 2)
-                oneprev = momfunc_orthopoly(n - 1)
-                twoprev_sqnorm = sqnorm(twoprev, momfunc)
-                oneprev_sqnorm = sqnorm(oneprev, momfunc)
-                product = inner(oneprev, x*oneprev, momfunc)
+                twoprev = self.orthopoly(n - 2)
+                oneprev = self.orthopoly(n - 1)
+                twoprev_sqnorm = self.sqnorm(twoprev)
+                oneprev_sqnorm = self.sqnorm(oneprev)
+                product = self.inner(oneprev, x*oneprev)
                 alpha = product/oneprev_sqnorm
                 beta = oneprev_sqnorm/twoprev_sqnorm
                 poly = (x - alpha)*oneprev - beta*twoprev
