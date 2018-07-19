1441568cd9840544dba3c05df08acd813bc223a4
Ross Jennings <rossjjennings@gmail.com>
2018-07-19 16:40:33 -0400

    Fix ac_6pt(); use Cayley-Menger determinant to compute area, allowing triangles not in a plane

--- cubit/triangle.py
+++ cubit/triangle.py
@@ -8,7 +8,7 @@ def from_barycentric(nodes, weights, vertices):
     '''
     Compute nodes and weights of a cubature rule on a triangle with
     specified vertices from the barycentric coordinates of the nodes and 
-    the relative weights. The triangle must lie in a plane.
+    the relative weights. The triangle need not lie in a plane.
     
     `nodes` should contain the barycentric coordinates of the nodes as 
             an n×3 array, where `n` is the number of nodes.
@@ -19,9 +19,33 @@ def from_barycentric(nodes, weights, vertices):
     a one-dimensional array of length n containing the weights
     (summing to the area of the triangle).
     '''
-    R = np.array([np.concatenate((vertex, np.array([1.]))) for vertex in vertices])
-    area = 1/2*np.linalg.det(R)
+    sqnorms = np.sum(vertices**2, axis=1)
+    cm_matrix = np.ones((4, 4)) # For computing Cayley-Menger determinant
+    cm_matrix[-1,-1] = 0
+    cm_matrix[:-1,:-1] = sqnorms + sqnorms[:,np.newaxis] - 2*vertices @ vertices.T
+    area = 1/4*np.sqrt(-np.linalg.det(cm_matrix))
     return (nodes @ vertices).T, weights*area
+
+def centroid(m=1):
+    '''
+    Centroid rule (Stroud Tn: 1-1 for n=2):
+    
+    The analog of the midpoint rule for triangles.
+    A first-degree rule using only the value at the centroid.
+    When tiled with m divisions per side, it uses m*(m+1)/2 points
+    to integrate over the m*(m+1)/2 sub-triangles.
+    '''
+    
+
+def wedge(m=1):
+    '''
+    Wedge rule (Stroud Tn: 1-2 for n=2):
+    
+    The analog of the trapezoid rule for triangles.
+    A first-degree rule using the values at the vertices.
+    When tiled with m divisions per side, it uses (m+1)*(m+2)/2 points
+    to integrate over the m*(m+1)/2 sub-triangles.
+    '''
 
 def ac_6pt():
     '''
@@ -38,9 +62,9 @@ def ac_6pt():
     B = 1/30
     C = 9/30
     
-    nodes =  np.array([[r, r, s, u, u, v],
-                       [r, s, r, u, v, u],
-                       [s, r, r, v, u, u]])
+    nodes =  np.array([[r, r, 0, u, u, v],
+                       [r, 0, r, u, v, u],
+                       [0, r, r, v, u, u]])
     weights = np.array([B, B, B, C, C, C])
     
     return from_barycentric(nodes, weights, vertices)
