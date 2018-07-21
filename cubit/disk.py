e985c116ad601d3aaf34bc702cab2ab28fe59412
Ross Jennings <rossjjennings@gmail.com>
2018-07-20 22:45:22 -0400

    Finish implementing ninth-degree rules

--- cubit/disk.py
+++ cubit/disk.py
@@ -168,16 +168,132 @@ def albrecht_19pt():
     return (x_nodes, y_nodes), pi*weights
 
 def rr_20pt():
-    pass
+    '''
+    Rabinowitz-Richter 20-point rule (Stroud S2: 9-2):
+    
+    A ninth-degree rule with 20 points. Four points are outside the unit disk.
+    
+    Rabinowitz, P., and Richter, N., "Perfectly symmetric two-dimensional
+    integration formulas with minimal numbers of points", Math. Comput.,
+    v. 23, 1969, pp. 765-779.
+    '''
+    r1 = 0.8377170225998396
+    r2 = 0.3924393142315810
+    r3 = 0.5505609906724360
+    r4 = 0.4249164962326038
+    s4 = 0.9112013890413142
+    B1 = 0.1851958765246450
+    B2 = 0.2930225148631698
+    B3 = 0.2296152967116376
+    
+    x_nodes = np.array([  r1,  r2,  r3,  r4,  s4,   0,   0, -r3, -s4, -r4,
+                         -r1, -r2, -r3, -r4, -s4,   0,   0,  r3,  s4,  r4])
+    y_nodes = np.array([   0,   0,  r3,  s4,  r4,  r1,  r2,  r3,  r4,  s4,
+                           0,   0, -r3, -s4, -r4, -r1, -r2, -r3, -r4, -s4])
+    weights = np.array([  B1,  B2,  B3,  B4,  B4,  B1,  B2,  B3,  B4,  B4,
+                          B1,  B2,  B3,  B4,  B4,  B1,  B2,  B3,  B4,  B4])
+    
+    return (x_nodes, y_nodes), pi*weights
 
 def lyusternik_21pt():
-    pass
+    '''
+    Lyusternik 21-point rule (Stroud S2: 9-3):
+    
+    A ninth-degree rule with 21 points. One point is the origin and the other
+    20 are the vertices of two regular decagons. This is also a spherical 
+    product Gauss rule. This is Stroud's corrected version of the rule,
+    which is given incorrectly by Lyusternik.
+    
+    Lyusternik, L. A., "Certain cubature formulas for double integrals"
+    (in Russian), Dokl. Akad. Nauk SSSR, v. 62, 1948, pp. 449-452. MR 10, 330.
+    '''
+    T = pi/5
+    r1 = sqrt((6-sqrt(6))/10)
+    r2 = sqrt((6+sqrt(6))/10)
+    B0 = 1/9
+    B1 = (16+sqrt(6))/360
+    B2 = (16-sqrt(6))/360
+    
+    x_nodes = np.array([0,  r1,  r1*cos(T),  r1*cos(2*T), -r1*cos(2*T), -r1*cos(T),
+                           -r1, -r1*cos(T), -r1*cos(2*T),  r1*cos(2*T),  r1*cos(T),
+                            r2,  r2*cos(T),  r2*cos(2*T), -r2*cos(2*T), -r2*cos(T),
+                           -r2, -r2*cos(T), -r2*cos(2*T),  r2*cos(2*T),  r2*cos(T)])
+    y_nodes = np.array([0,   0,  r1*sin(T),  r1*sin(2*T),  r1*sin(2*T),  r1*sin(T),
+                             0, -r1*sin(T), -r1*sin(2*T), -r1*sin(2*T), -r1*sin(T),
+                             0,  r2*sin(T),  r2*sin(2*T),  r2*sin(2*T),  r2*sin(T),
+                             0, -r2*sin(T), -r2*sin(2*T), -r2*sin(2*T), -r2*sin(T)])
+    weights = np.array([B0, B1,         B1,           B1,           B1,         B1,
+                            B1,         B1,           B1,           B1,         B1,
+                            B2,         B2,           B2,           B2,         B2,
+                            B2,         B2,           B2,           B2,         B2])
+    
+    return (x_nodes, y_nodes), pi*weights
 
 def rr_21pt():
-    pass
+    '''
+    Rabinowitz-Richter 21-point rule (Stroud S2: 9-5):
+    
+    A ninth-degree rule with 21 points. Four points are outside the unit disk.
+    
+    Rabinowitz, P., and Richter, N., "Perfectly symmetric two-dimensional
+    integration formulas with minimal numbers of points", Math. Comput.,
+    v. 23, 1969, pp. 765-779.
+    '''
+    r1 = 0.9499490053854548
+    r2 = 0.4184300297249359
+    r3 = 0.8485281374238570
+    r4 = 0.3830079234911947
+    s4 = 0.7409163950514299
+    A  = 0.0043173954188430
+    B1 = 0.0734867016303473
+    B2 = 0.3295210136662689
+    B3 = 0.0046091399966757
+    B4 = 0.1883509796247228
+    
+    x_nodes = np.array([0,  r1,  r2,  r3,  r4,  s4,   0,   0, -r3, -s4, -r4,
+                           -r1, -r2, -r3, -r4, -s4,   0,   0,  r3,  s4,  r4])
+    y_nodes = np.array([0,   0,   0,  r3,  s4,  r4,  r1,  r2,  r3,  r4,  s4,
+                             0,   0, -r3, -s4, -r4, -r1, -r2, -r3, -r4, -s4])
+    weights = np.array([A,  B1,  B2,  B3,  B4,  B4,  B1,  B2,  B3,  B4,  B4,
+                            B1,  B2,  B3,  B4,  B4,  B1,  B2,  B3,  B4,  B4])
+    
+    return (x_nodes, y_nodes), pi*weights
 
 def peirce_28pt():
-    pass
+    '''
+    Peirce's 28-point rule
+    
+    A ninth-degree rule with 28 points.
+    
+    Peirce, W. H., "Numerical Integration over Planar Regions",
+    Ph.D. thesis, University of Wisconsin-Madison, 1956.
+    '''
+    r  = sqrt((5+sqrt(15))/10)
+    u1 = sqrt((5-sqrt(15))/10)*cos(pi/8)
+    v1 = sqrt((5-sqrt(15))/10)*sin(pi/8)
+    u2 = sqrt(1/2)*cos(pi/8)
+    v2 = sqrt(1/2)*sin(pi/8)
+    u3 = sqrt((5+sqrt(15)+sqrt(185*sqrt(15)-700))/20)
+    v3 = sqrt((5+sqrt(15)-sqrt(185*sqrt(15)-700))/20)
+    B1 = (12060-1440*sqrt(15))/254088
+    B2 = 5/144
+    B3 = 1/18
+    B4 = (5585+1440*sqrt(15))/508176
+    
+    x_nodes = np.array([ r,  u1,  v1,  u2,  v2,  u3,  v3,
+                         0, -v1, -u1, -v2, -u2, -v3, -u3,
+                        -r, -u1, -v1, -u2, -v2, -u3, -v3,
+                         0,  v1,  u1,  v2,  u2,  v3,  u3])
+    y_nodes = np.array([ 0,  v1,  u1,  v2,  u2,  v3,  u3,
+                         r,  u1,  v1,  u2,  v2,  u3,  v3,
+                         0, -v1, -u1, -v2, -u2, -v3, -u3,
+                        -r, -u1, -v1, -u2, -v2, -u3, -v3])
+    weights = np.array([B1,  B2,  B2,  B3,  B3,  B4,  B4,
+                        B1,  B2,  B2,  B3,  B3,  B4,  B4,
+                        B1,  B2,  B2,  B3,  B3,  B4,  B4,
+                        B1,  B2,  B2,  B3,  B3,  B4,  B4])
+    
+    return (x_nodes, y_nodes), pi*weights
 
 def mysovskikh_28pt():
     pass
