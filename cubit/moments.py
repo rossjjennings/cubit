4299f2d60752a2acef69fe11a9d5dcdde985fb24
Ross Jennings <rossjjennings@gmail.com>
2019-05-29 14:52:13 -0400

    Fix weight_fn method arguments

--- cubit/moments.py
+++ cubit/moments.py
@@ -1,26 +1,28 @@
 import sympy as sym
 
 x = sym.Symbol('x')
+
 class weight_fn:
     def __init__(self, momfunc):
         self.moment = momfunc
+        self._cache = dict()
     
-    def inner(p1, p2):
+    def inner(self, p1, p2):
         prod = sym.Poly(p1*p2, x)
         iter = enumerate(reversed(prod.all_coeffs()))
         return sum(coeff*self.moment(i) for i, coeff in iter)
     
-    def sqnorm(p):
+    def sqnorm(self, p):
         return self.inner(p, p)
     
-    def orthopoly(self, n, cache = {}):
+    def orthopoly(self, n):
         try:
-            return cache[n]
+            return self._cache[n]
         except KeyError:
             if n == 0:
-                cache[n] = sym.Integer(1)
+                self._cache[n] = sym.Integer(1)
             elif n == 1:
-                cache[n] = x - self.moment(1)/moment(0)
+                self._cache[n] = x - self.moment(1)/self.moment(0)
             else:
                 twoprev = self.orthopoly(n - 2)
                 oneprev = self.orthopoly(n - 1)
@@ -30,6 +32,5 @@ class weight_fn:
                 alpha = product/oneprev_sqnorm
                 beta = oneprev_sqnorm/twoprev_sqnorm
                 poly = (x - alpha)*oneprev - beta*twoprev
-                cache[n] = sym.expand(poly)
-            return cache[n]
-    return momfunc_orthopoly
+                self._cache[n] = sym.expand(poly)
+            return self._cache[n]
